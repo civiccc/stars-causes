ecba5c22916b1de7817fd2cd975b38f1e673c6c7
Joe Lencioni <joe.lencioni@causes.com>
2012-08-31 19:58:26 -0700

    Add names to user photos
    When using Stars, it can be difficult to know who a particular user
    photo belongs to. This commit adds names to user photos via title
    attributes, which should appear as tooltips when hovering.
    
    Change-Id: I938239a3644c4a99b3eeb38109accc3ca8fe3246
    Reviewed-on: https://gerrit.causes.com/11921
    Reviewed-by: Kim Douglas Clarence Mason <kim.mason@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Kim Douglas Clarence Mason <kim.mason@causes.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def second_button(star)
     if current_user.can_second?(star)
-      link_to "Second", star_seconds_url(star), :class => "second",
+      link_to 'Second', star_seconds_url(star), :class => 'second',
         'data-remote' => true, 'data-replace-id' => "star_#{star.id}",
         'data-method' => 'post'
     end
@@ -18,16 +18,17 @@ module ApplicationHelper
   def photo(user)
     image_url =
       "http://graph.facebook.com/#{user.facebook_uid.to_i}/picture?type=square"
-    link_to image_tag(image_url, :class => "user_photo", :alt => ""),
+    link_to image_tag(image_url, :class => 'user_photo', :alt => '',
+                      :title => user.name)
       user_url(user)
   end
 
   def background_photo(user)
     image_url =
       "http://graph.facebook.com/#{user.facebook_uid.to_i}/picture?type=large"
-    div = content_tag(:div, "", :class => "user_photo", :style =>
-      "background-image: url('#{image_url}');
-       background-position: center; background-size: cover")
+    div = content_tag(:div, '', :class => 'user_photo', :title => user.name,
+                      :style => "background-image: url('#{image_url}');" +
+                        'background-position: center; background-size: cover')
     link_to div, user_path(user)
   end
 
